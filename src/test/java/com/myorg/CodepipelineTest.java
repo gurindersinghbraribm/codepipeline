4828fa895fb768dd1ededbef4e69c2d401d3877a
Gurinder Brar <gurinder.brar-cic@ibm.com>
2022-05-09 15:47:06 +0100

    update

--- src/test/java/com/myorg/CodepipelineTest.java
+++ src/test/java/com/myorg/CodepipelineTest.java
@@ -1,26 +1,28 @@
-// package com.myorg;
+package com.myorg;
 
-// import software.amazon.awscdk.App;
-// import software.amazon.awscdk.assertions.Template;
-// import java.io.IOException;
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.assertions.Template;
 
-// import java.util.HashMap;
+import java.io.IOException;
 
-// import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Test;
 
 // example test. To run these tests, uncomment this file, along with the
-// example resource in java/src/main/java/com/myorg/CodepipelineStack.java
-// public class CodepipelineTest {
+//example resource in java/src/main/java/com/myorg/CodepipelineStack.java
+public class CodepipelineTest {
 
-//     @Test
-//     public void testStack() throws IOException {
-//         App app = new App();
-//         CodepipelineStack stack = new CodepipelineStack(app, "test");
+    @Test
+    public void testStack() throws IOException {
 
-//         Template template = Template.fromStack(stack);
+        App app = new App();
+        CodePipelineStack stack = new CodePipelineStack(app, "test");
 
-//         template.hasResourceProperties("AWS::SQS::Queue", new HashMap<String, Number>() {{
-//           put("VisibilityTimeout", 300);
-//         }});
-//     }
-// }
+        Template template = Template.fromStack(stack);
+
+        template.hasResourceProperties("AWS::SQS::Queue", new HashMap<String, Number>() {{
+            put("VisibilityTimeout", 300);
+        }});
+    }
+}
